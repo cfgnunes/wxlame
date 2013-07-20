257227cc71e20c0ff8607fcf4c655cf609c1d0da
Cristiano Nunes <cfgnunes@gmail.com>
2013-07-20 00:40:19 +0000

    Fix adding duplicates files in list Added file counter in statusbar

--- wxlame/src/FileInfo.h
+++ wxlame/src/FileInfo.h
@@ -1,5 +1,5 @@
 /*
- * This file is part of the wxMP3gain and licensed under the GNU General Public License, version 3
+ * This file is part of the wxLame and licensed under the GNU General Public License, version 3
  * http://www.gnu.org/licenses/gpl-3.0.html
  */
 
